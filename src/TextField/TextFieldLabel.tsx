96b290556592eb0431da8539f6d10c7aaa0a2eab
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-20 19:15:11 +0500

    chore: remove TextField

--- src/TextField/TextFieldLabel.tsx
+++
@@ -1,5 +0,0 @@
-import type { JSX } from 'solid-js';
-
-export function TextFieldLabel(props: JSX.IntrinsicElements['p']) {
-  return <p {...props} class="text-field__label"/>;
-}
--- src/TextField/TextFieldPlaceholder.tsx
+++
@@ -1,5 +0,0 @@
-import type { JSX } from 'solid-js';
-
-export function TextFieldPlaceholder(props: JSX.IntrinsicElements['p']) {
-  return <p {...props} class="text-field__placeholder"/>;
-}
