dabab98908fb082cbe041c25b40875947c384c9e
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-20 23:26:56 +0500

    chore(App): rename to AppPlatformed

--- src/App/App.platformed.ts
+++ src/App/App.platformed.ts
@@ -3,7 +3,7 @@ import { platformed } from 'virtual:platformed';
 import { AppCommon } from '@/App/App.common.js';
 import { AppIos } from '@/App/App.ios.js';
 
-export const App = platformed({
+export const AppPlatformed = platformed({
   common: AppCommon,
   ios: AppIos,
 });
