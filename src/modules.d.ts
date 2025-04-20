98cd8aeececcc53d5d2de584d4ce1e63e7822b7c
Vladislav Kibenko <34907325+heyqbnk@users.noreply.github.com>
2025-04-20 23:20:55 +0500

    fix: bring virtual module back

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,7 +1,8 @@
-type PlatformID = 'ios' | 'android';
-
-declare function platformed<P>(
-  overrides: {
-    common: import('solid-js').Component<P>;
-  } & Partial<Record<PlatformID, import('solid-js').Component<P>>>,
-): import('solid-js').Component<P>;
+declare module "virtual:platformed" {
+  export type PlatformID = 'ios' | 'android';
+  export function platformed<P>(
+    overrides: {
+      common: import('solid-js').Component<P>;
+    } & Partial<Record<PlatformID, import('solid-js').Component<P>>>,
+  ): import('solid-js').Component<P>;
+}
